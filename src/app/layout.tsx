a1b699d22c797da460c6b623c862e8db5c6d1e81
zhaoyujie2025 <zhaozong2024@gmail.com>
2025-09-15 23:47:14 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default async function RootLayout({
 }) {
   const storageType = process.env.NEXT_PUBLIC_STORAGE_TYPE || 'localstorage';
 
-  let siteName = process.env.NEXT_PUBLIC_SITE_NAME || 'OrangeTV';
+  let siteName = process.env.NEXT_PUBLIC_SITE_NAME || '南瓜影院';
   let announcement =
     process.env.ANNOUNCEMENT ||
     '本网站仅提供影视信息搜索服务，所有内容均来自第三方网站。本站不存储任何视频资源，不对任何内容的准确性、合法性、完整性负责。';
